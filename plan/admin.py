06ac6afb9c11ab41ac340e379fa6d75b571f79fd
eandablo <>
2023-11-10 14:39:13 +0000

    register TraineeInfo in admin.py

--- plan/admin.py
+++ plan/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TraineeInfo, Exercises, WorkoutLog
 
 # Register your models here.
+
+
+@admin.register(TraineeInfo)
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'updated_date', 'age', 'weight', 'height')
+    list_filter = ('name',)
