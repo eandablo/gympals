e489c4ccfe61412057eb67aae38815c337bd0488
eandablo <>
2023-11-10 16:50:36 +0000

    create function in index page to add Trainee_info instance

--- plan/views.py
+++ plan/views.py
@@ -16,3 +16,17 @@ class HomeView(View):
             {"info_exists": info_exists,
              "info_form": TraineeInfoForm()}
         )
+
+    def post(self, request, *args, **kwargs):
+        info_form = TraineeInfoForm(data=request.POST)
+        new_trainee = request.user
+        info_exists = False
+        if info_form.is_valid():
+            info_form.instance.trainee = new_trainee
+            info_form.save()
+            info_exists = True
+        return render(
+            request,
+            'index.html',
+            {"info_exists": info_exists}
+        )
