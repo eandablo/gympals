5103412d3244ed670d6502bc0c7802f330cfc05b
eandablo <>
2023-11-10 20:10:37 +0000

    create LogExerciseForm to accept reps, sets, completed

--- plan/forms.py
+++ plan/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import TraineeInfo
+from .models import TraineeInfo, WorkoutLog
 
 
 class TraineeInfoForm(forms.ModelForm):
     class Meta:
         model = TraineeInfo
         fields = ('name', 'age', 'weight', 'height')
+
+
+class LogExerciseForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutLog
+        fields = ('sets_actual', 'reps_actual', 'completed')
