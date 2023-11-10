7fcd043778bb89bafb97af9e08c5994c361d97bc
eandablo <>
2023-11-10 17:29:44 +0000

    add

--- plan/admin.py
+++ plan/admin.py
@@ -8,3 +8,9 @@ from .models import TraineeInfo, Exercises, WorkoutLog
 class InfoAdmin(admin.ModelAdmin):
     list_display = ('name', 'updated_date', 'age', 'weight', 'height')
     list_filter = ('name',)
+
+
+@admin.register(Exercises)   
+class ExerciseInfo(admin.ModelAdmin):
+    list_display = ('name', 'guide_image', 'muscle_group', 'youtube_link')
+    list_filter = ('muscle_group',)
