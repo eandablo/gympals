2b30e885dcd393fb7f4b1895f72c0afa2e0adf6b
Efren Andablo <eandablo@gmail.com>
2023-11-16 16:28:55 +0000

    add dummy variable to workout_plan view to test external variable

--- plan/decisions.py
+++ plan/decisions.py
@@ -3,20 +3,20 @@ import random
 
 class WorkoutMaker():
     def select_ids():
-        days = [{
-                'day':1,
-                'groups': ['BACK', 'BICEPS']}
-                {'day':2,
-                'groups': ['CHEST', 'TRICEPS']},
-                # {'day':3,
-                # 'groups': ['LEGS', 'SHOULDERS'],}
-            ]
-        all_ids = []
-        for day in days:
-            groups = day['groups']
-            for group in groups:
-                exercise_list = Exercises.objects.filter(muscle_group=group)
-                id_list = exercise_list.values_list('id')
-                random_list = random.sample(id_list, k=3)
-                all_ids.extend(id_list)
-        return all_ids
+        # days = [{
+        #         'day':1,
+        #         'groups': ['BACK', 'BICEPS']}
+        #         {'day':2,
+        #         'groups': ['CHEST', 'TRICEPS']},
+        #         # {'day':3,
+        #         # 'groups': ['LEGS', 'SHOULDERS'],}
+        #     ]
+        # all_ids = []
+        # for day in days:
+        #     groups = day['groups']
+        #     for group in groups:
+        #         exercise_list = Exercises.objects.filter(muscle_group=group)
+        #         id_list = exercise_list.values_list('id')
+        #         random_list = random.sample(id_list, k=3)
+        #         all_ids.extend(id_list)
+        return 1
--- plan/views.py
+++ plan/views.py
@@ -3,6 +3,7 @@ from django.views import View, generic
 from .forms import TraineeInfoForm, LogExerciseForm
 from .models import TraineeInfo, WorkoutLog
 from django.http import HttpResponseRedirect
+from .decisions import WorkoutMaker
 
 class HomeView(View):
 
@@ -39,9 +40,12 @@ class HomeView(View):
         )
 
 
-class WorkoutView(View):
+class WorkoutView(View, WorkoutMaker):
     def get(self, request, name, *args, **kwargs):
         logs = WorkoutLog.objects.filter(trainee__name=name, completed=False)
+        dummy = 0
+        if not WorkoutLog.objects.filter(trainee__name=name, completed=False):
+            dummy = self.select_ids
         days = logs.order_by('day').values_list('day').distinct('day')
         ids = []
         for day in days:
@@ -53,7 +57,8 @@ class WorkoutView(View):
             'workout_plan.html',
             {"logs": logs,
              "ids": ids,
-             "update_form": LogExerciseForm()
+             "update_form": LogExerciseForm(),
+             "dummy": dummy
             }
         )
 
