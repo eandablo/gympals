32760ca8a749d1c4447437737a5784c28de5489a
Efren Andablo <eandablo@gmail.com>
2023-11-20 16:52:59 +0000

    add base.html to sign in page

--- trainer/settings.py
+++ trainer/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-eandablo-gympals-fm9k9dzrc67.ws-eu106.gitpod.io',
+ALLOWED_HOSTS = ['8000-eandablo-gympals-tkg42z3fjtf.ws-eu106.gitpod.io',
                  'gympals-6af3102877bb.herokuapp.com']
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
