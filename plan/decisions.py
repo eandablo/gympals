4cb45c15158311159f8b591aebf8832a8a5e8d39
eandablo <>
2023-11-22 10:41:10 +0000

    create Diete

--- plan/decisions.py
+++ plan/decisions.py
@@ -32,3 +32,22 @@ class WorkoutGen():
                         excercise=get_object_or_404(Exercises, id=id)
                     )
         return 'week1' + trainee.name + str(day['day'])
+
+
+class DietGen():
+    def calories_calc(self, name):
+        trainee = get_object_or_404(TraineeInfo, name=name)
+        goal = trainee.goal
+        sex = trainee.sex
+        weight = trainee.weight
+        height = trainee.height
+        age = trainee.age
+        if sex == 'female':
+            basal = 66.5 + 9.5634 * weight + 1.85 * height - 4.676 * age
+        elif sex == 'male':
+            basal = 66.5 + 13.75 * weight + 5.003 * height - 6.775 * age
+        else:
+            basalM = 66.5 + 13.75 * weight + 5.003 * height - 6.775 * age
+            basalF = 66.5 + 9.5634 * weight + 1.85 * height - 4.676 * age
+            basal = (basalM + basalF)
+        
