1974952d9dfc1c02bf2c1fa555b16b26fd3f9e9b
Efren Andablo <eandablo@gmail.com>
2023-11-23 09:53:03 +0000

    set interval to welcome dashboard to display calories

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded',function(){
-    button = document.getElementById('alert-button');
-    let messages = document.getElementById("bootstrap-msg");
-    let alert = new bootstrap.Alert(messages);
-    button.addEventListener('click', function(){
-      alert.close();
-    });
-    setInterval(function(){
-        alert.close();
-    }, 5000);
+document.addEventListener('DOMContentLoaded', function(){
+  alertButton = document.getElementById("alert-button");
+  let messages = document.getElementById("bootstrap-msg");
+  let alert = new bootstrap.Alert(messages);
+  alertButton.addEventListener('click', function(){
+    alert.close();
+  });
+  setTimeout(function(){
+    alert.close();
+  }, 5000);
+  calDiv = document.getElementById("cal-container");
+  setInterval(function(){
+    if (calDiv.style.color === "rgb(247, 247, 247)"){
+      calDiv.style.color = "rgb(155, 0, 0)";
+    } else {
+      calDiv.style.color = "rgb(247, 247, 247)";
+    }
+  }, 1000);
 });
