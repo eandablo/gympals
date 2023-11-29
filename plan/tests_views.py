7a6daf32e7c063cc15dfc3536e13ce0aa411fa15
eandablo <>
2023-11-29 15:26:43 +0000

    add first funtion TestHome in test_views

--- plan/tests_views.py
+++ plan/tests_views.py
@@ -1,2 +1,8 @@
 from django.test import TestCase
 
+
+class TestHome(TestCase):
+    def test_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
