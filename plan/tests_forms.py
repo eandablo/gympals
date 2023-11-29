2c93948447f6db2e3ae3ace98093f13440e2bb0f
eandablo <>
2023-11-29 11:32:18 +0000

    add TestUpdateInfoForm to test UpdateInfoForm fields are required

--- plan/tests_forms.py
+++ plan/tests_forms.py
@@ -54,3 +54,40 @@ class TestTraineeInfoForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('height', form.errors.keys())
         self.assertEqual(form.errors['height'][0], 'This field is required.')
+
+
+class TestUpdateInfoForm(TestCase):
+    def test_age_field_is_required(self):
+        form = forms.UpdateInfoForm(
+            {'age': '',
+             'weight': 3,
+             'height': 4,
+             'sex': 'F',
+             'goal': 'WL'}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('age', form.errors.keys())
+        self.assertEqual(form.errors['age'][0], 'This field is required.')
+
+    def test_weight_field_is_required(self):
+        form = forms.UpdateInfoForm(
+            {'age': 1,
+             'weight': '',
+             'height': 4,
+             'sex': 'F',
+             'goal': 'WL'}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('weight', form.errors.keys())
+        self.assertEqual(form.errors['weight'][0], 'This field is required.')
+
+    def test_height_field_is_required(self):
+        form = forms.UpdateInfoForm(
+            {'weight': 20,
+             'height': '',
+             'sex': 'F',
+             'goal': 'WL'}
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn('height', form.errors.keys())
+        self.assertEqual(form.errors['height'][0], 'This field is required.')
