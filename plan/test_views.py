9585fea75f43c9c3440c9c46df2b7b8d5c88f9ab
eandablo <>
2023-12-06 18:16:22 +0000

    add test for login page

--- plan/test_views.py
+++ plan/test_views.py
@@ -3,24 +3,17 @@ from django.test import Client
 from django.urls import reverse
 from . import models
 from django.contrib.auth.models import User
-    # def setUpTestData(cls):
-    #     print("setUpTestData: Run once to set up non-modified data for all class methods.")
-    #     pass
+# def setUpTestData(cls):
 
 
-def trainee_item():
-    '''
-    defining item for use in TestTraineeInfo
-    '''
-    user = User.objects.create(username='tester', password='kjhasdf')
-    item = models.TraineeInfo.objects.create(
-        trainee=user,
-        name='Person',
-        age=30,
-        weight=50,
-        height=50
-    )
-    return [user, item]
+class TestLogin(TestCase):
+    def test_login(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('yoyoyoyo')
+        user.save()
+        logged_in = self.client.login(username='testuser', password='yoyoyoyo')
+        print(logged_in)
+        print(user.is_authenticated)
 
 
 class TestHomeView(TestCase):
@@ -28,21 +21,3 @@ class TestHomeView(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-
-    def test_post_info_form(self):
-        user = trainee_item()
-        test_data = {'user': user[0],
-                     'name': 'tester',
-                     'age': 10,
-                     'weight': 10,
-                     'height': 10,
-                     'sex': 'M',
-                     'goal': 'WL'}
-        
-        response = self.client.post('/', test_data)
-
-
-class TestWorkoutView(TestCase):
-    def test_response(self):
-        item = trainee_item()
-        # self.client.get(f'/plan/{item.name}')
